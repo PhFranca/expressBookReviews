111eeea8d7e73699e9a2ddcf333f83a1a862dcd2
PhFranca <phlavio.franca@outlook.com>
2023-06-04 22:06:58 -0400

    Improving the scope of Tasks 1-4 using Promises or Async-Await to taks 10, 11, 12 and 13.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -21,31 +22,67 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    res.send(JSON.stringify({books},null,4));
+public_users.get('/', async function (req, res) {
+    try {
+      res.send({ books });
+    } catch (error) {
+      console.error("Error fetching books:", error.message);
+      res.status(500).json({ message: "Failed to fetch books", error: error.message });
+    }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(books[isbn])
- });
+public_users.get('/isbn/:isbn', async function (req, res) {
+    try {
+      const isbn = req.params.isbn;
+      if (books.hasOwnProperty(isbn)) {
+        res.send(books[isbn]);
+      } else {
+        res.status(404).json({ message: "Book not found" });
+      }
+    } catch (error) {
+      console.error("Error fetching book details:", error.message);
+      res.status(500).json({ message: "Failed to fetch book details", error: error.message });
+    }
+  });
+  
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    let booksArray = Object.values(books);
-    let filtered_books = booksArray.filter((book) => book.author === author);
-    res.send(filtered_books);
-});
+public_users.get('/author/:author', async function (req, res) {
+    try {
+      const author = req.params.author;
+      let booksArray = Object.values(books);
+      let filteredBooks = booksArray.filter((book) => book.author === author);
+      
+      if (filteredBooks.length > 0) {
+        res.send(filteredBooks);
+      } else {
+        res.status(404).json({ message: "No books found for the author" });
+      }
+    } catch (error) {
+      console.error("Error fetching books by author:", error.message);
+      res.status(500).json({ message: "Failed to fetch books by author", error: error.message });
+    }
+  });
+  
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    let booksArray = Object.values(books);
-    let filtered_books = booksArray.filter((book) => book.title === title);
-    res.send(filtered_books);
-});
+public_users.get('/title/:title', async function (req, res) {
+    try {
+      const title = req.params.title;
+      let booksArray = Object.values(books);
+      let filteredBooks = booksArray.filter((book) => book.title === title);
+  
+      if (filteredBooks.length > 0) {
+        res.send(filteredBooks);
+      } else {
+        res.status(404).json({ message: "No books found with the specified title" });
+      }
+    } catch (error) {
+      console.error("Error fetching books by title:", error.message);
+      res.status(500).json({ message: "Failed to fetch books by title", error: error.message });
+    }
+  });
 
 //  Get book review
     public_users.get('/review/:isbn', function (req, res) {
